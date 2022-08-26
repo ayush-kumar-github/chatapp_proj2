962a1ba2cc60274590b7d5813cf685f8ba2abbe7
Ayush Kumar <aykr571@gmail.com>
2022-08-26 19:02:07 +0530

    deploy it better

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,12 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+__dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
